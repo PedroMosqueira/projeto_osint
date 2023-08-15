f1fbccd3908b77624bd0e0a4d4b18ad17f9145c6
Pedro Mosqueira <pedromosqueira.00@gmail.com>
2023-08-15 20:30:25 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,108 @@
+
+var valor=document.getElementById("imodo_b").value;
+document.getElementById("imodo_bt").value=String(valor);
+
+ function val(){
+    var valor=document.getElementById("imodo_b").value;
+document.getElementById("imodo_bt").value=String(valor);
+
+
+
+ }
+ function nome(n){
+    
+    document.getElementById('imodo_bt').name=String(n);
+
+
+ }
+
+
+
+
+function desm(){
+    document.getElementById("divb").style.backgroundColor='rgba(255, 255, 255, 0.062)';
+    document.getElementById("divb").style.color='white';
+
+    document.getElementById("divv").style.backgroundColor='rgba(255, 255, 255, 0.062)';
+    document.getElementById("divv").style.color='white';
+
+    document.getElementById("divc").style.backgroundColor='rgba(255, 255, 255, 0.062)';
+    document.getElementById("divc").style.color='white';
+
+
+
+
+}
+
+
+
+
+function selecionar(id){
+
+    desm();
+   
+    
+    document.getElementById(id).style.backgroundColor='white';
+    document.getElementById(id).style.color='black';
+
+    
+    document.getElementById('dd').style.display='initial';
+    document.getElementById('tk').style.display='initial';
+
+    switch(id){
+
+        case 'divb':
+            document.getElementById('linkb').href='tokenb.html';
+            break;
+        
+        case 'divv' :
+            document.getElementById('linkb').href='tokenv.html';
+            break;
+        case 'divc' :
+            document.getElementById('linkb').href='tokenc.html';
+            break;
+
+ 
+    }
+
+    
+
+   
+ 
+
+
+
+}
+
+function dados(){
+
+
+
+    document.getElementById('inivel').style.display='none';
+    document.getElementById("subm").style.display='block';
+
+    var d= document.querySelector('input[name="nivel"]:checked').value;
+
+    
+    
+    if(d=='basico'){
+        document.getElementById('op_b').style.display='block';
+        
+
+    }else if(d=='veiculo'){
+        document.getElementById('op_v').style.display='block';
+        
+
+
+    }else if(d=='completo'){
+        document.getElementById('op_c').style.display='block';
+}
+
+}
+
+
+
+
+
+
+
